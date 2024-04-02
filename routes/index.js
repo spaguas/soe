521c2bec70ef2479e2ba97f566bbcdc28e0dd862
Diego Monteiro <dmsburn@gmail.com>
2024-04-02 11:23:32 -0300

    Ajustando exibição e formulários

--- routes/index.js
+++ routes/index.js
@@ -206,58 +206,10 @@ router.get('/', function(req, res, next) {
             console.error('Error cleaning up scroll context:', error);
           });
       }
-
-      // Call the scroll request after the initial search request
-      
-      
-      
-      // const rst = await elasticClient.search({
-      //   index: 'outorgas',
-      //   // keep the search results "scrollable" for 30 seconds
-      //   scroll: '10s',
-      //   // for the sake of this example, we will get only one result per search
-      //   size: 10,
-      //   // filter the source to only include the quote field
-      //   //_source: ['uid','latitude','longitude','decisao_pto','diretoria_req','finalidade_1630'],
-      //   query: {
-      //     bool:{
-      //       must: must_filter
-      //     }
-      //   }
-      // });
-
-      // console.log("Result: ",rst);
-      // rsts.push(rst);
-
-      // while(rsts.length){
-      //   const body = rsts.shift();
-
-      //   //console.log("Body: ", body);
-      //   body.hits.hits.forEach(function(hit){
-      //     //console.log(hit);
-      //     outorgas.push(hit);
-      //   });
-
-      //   // check to see if we have collected all of the quotes
-      //   if (body.hits.total.value === outorgas.length) {
-      //     //console.log('Outorgas Carregadas', outorgas);
-          
-      //     res.render('index', { title: 'SOE-DAEE', outorgas: JSON.stringify(outorgas).replaceAll("&#34;","\"") });
-      //     break
-      //   }
-
-      //   // get the next response if there are more quotes to fetch
-      //   rsts.push(
-      //     await elasticClient.scroll({
-      //       scroll_id: body._scroll_id,
-      //       scroll: '10s'
-      //     })
-      //   );
-
-      // }
     }
     else{
       //console.log("Selecione um municipio para continuar...");
+      //res.render('home/index', { title: 'SOE-DAEE', outorgas: "" });
       res.render('index', { title: 'SOE-DAEE', outorgas: "" });
     }
 
